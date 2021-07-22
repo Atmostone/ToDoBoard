44b9c6a0645ffe3fd7b7baa79ea7c3f3e3f08d1b
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-07-22 19:58:24 +0300

    Add React

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div><br/><i>This is footer</i></div>
+    )
+}
+export default Footer
---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div><i>This is menu</i> <br/></div>
+    )
+}
+export default Menu
