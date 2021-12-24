85bf5daac136d8e965135ecadb4bd16f8fce294b
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-12-24 11:41:25 +0900

    Add Swagger docs support

--- ToDoBoard/urls.py
+++ ToDoBoard/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from todo.views import ToDoViewSet, ProjectViewSet
@@ -11,6 +14,18 @@ router.register('users', UserModelViewSet)
 router.register('todos', ToDoViewSet)
 router.register('projects', ProjectViewSet)
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDoApp",
+      default_version='1',
+      description="Documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny, ),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
@@ -18,5 +33,9 @@ urlpatterns = [
     #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-token-auth/', views.obtain_auth_token),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 
 ]
